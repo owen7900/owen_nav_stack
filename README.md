6ab2f6e205603d07cec0874deb217a0c336af9be
Owen Hooper <17ofh@queensu.ca>
2021-11-22 20:41:06 -0500

    modified for slam functionality

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 This is my simple nav stack for an iRobot Create2.
 
 Over time, I plan to re-implement most 3rd party nodes and systems myself as programming exercises
+
+This now works with slam
+
+On PC
+`ros2 launch slam_toolbox online_async_launch.py`
+`rviz2`
+
+On Robot
+`ros2 launch create_bringup create_2.launch`
+`ros2 run system_controller system_controller_node`
+`ros2 launch rplidar_ros2 view_rplidar_launch.py serial_port:=/dev/ttyUSB1`
---
+++ owen_bringup/launch/simulation.launch.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+def generate_launch_description():
+
+  use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+  urdf_file_name = 'urdf/create_2.urdf.xacro'
+
+
+  urdf = os.path.join(
+      get_package_share_directory('create_description'),
+      urdf_file_name)
+
+  create_desc = get_package_share_directory('create_description')
+  print("urdf_file_name : {}".format(urdf_file_name))
+  return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH',value=create_desc+"/../"),
+
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                {'robot_description': xacro.process_file(urdf).toxml()}]
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='urdf_spawner',
+            output='screen',
+            arguments=["-topic", "/robot_description", "-entity", "create_2"])
+  ])
