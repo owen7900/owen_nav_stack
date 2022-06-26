1fd33cca8427624fdd2de4c640980aa82508a791
Owen Hooper <17ofh@queensu.ca>
2022-06-26 13:12:04 -0400

    Added manual control to system controller

--- system_controller/include/system_controller/SystemController.hpp
+++ system_controller/include/system_controller/SystemController.hpp
@@ -10,6 +10,13 @@ struct RobotStatus
   create_msgs::msg::Cliff cliff;
 };
 
+enum class ControlSource
+{
+  Manual,
+  Autonomous,
+  Stopped
+};
+
 class SystemController : public rclcpp::Node
 {
 public:
@@ -19,11 +26,18 @@ private:
   void bumperCallback(const create_msgs::msg::Bumper::ConstSharedPtr msg);
   void cliffCallback(const create_msgs::msg::Cliff::ConstSharedPtr msg);
   void cmdVelCallback(const geometry_msgs::msg::Twist::ConstSharedPtr msg);
+  void manualCmdVelCallback(const geometry_msgs::msg::Twist::ConstSharedPtr msg);
 
+  void controlCallback();
 private:
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _cmdVelPub;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _cmdVelSub;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _manualSub;
   rclcpp::Subscription<create_msgs::msg::Bumper>::SharedPtr _bumperSub;
   rclcpp::Subscription<create_msgs::msg::Cliff>::SharedPtr _cliffSub;
+  rclcpp::WallTimer<std::function<void()>>::SharedPtr _timer;
+  owen_common::Timestamp<geometry_msgs::msg::Twist> _autonomousCmd;
+  owen_common::Timestamp<geometry_msgs::msg::Twist> _manualCmd;
   owen_common::Timestamp<RobotStatus> _status;
+  ControlSource _controlSource;
 };
--- system_controller/src/SystemController.cpp
+++ system_controller/src/SystemController.cpp
@@ -1,5 +1,7 @@
 #include "system_controller/SystemController.hpp"
 #include <algorithm>
+#include <chrono>
+#include <ratio>
 
 constexpr double WarningSpeed = 0.05;
 constexpr uint16_t WarningIntensity = 100;
@@ -13,6 +15,9 @@ SystemController::SystemController(const std::string& name) : rclcpp::Node(name)
   this->_cmdVelSub = this->create_subscription<geometry_msgs::msg::Twist>(
       "/cmd_vel", 10, std::bind(&SystemController::cmdVelCallback, this, _1));
 
+  this->_manualSub = this->create_subscription<geometry_msgs::msg::Twist>(
+      "/manual/cmd_vel", 10, std::bind(&SystemController::manualCmdVelCallback, this, _1));
+
   this->_bumperSub = this->create_subscription<create_msgs::msg::Bumper>(
       "/roomba/bumper", 10, std::bind(&SystemController::bumperCallback, this, _1));
 
@@ -23,7 +28,22 @@ SystemController::SystemController(const std::string& name) : rclcpp::Node(name)
   double statusTimeout;
   this->get_parameter("status_timeout", statusTimeout);
   this->_status.SetTimeout(rclcpp::Duration::from_seconds(statusTimeout));
-  RCLCPP_INFO(get_logger(), "ON CONFIG");
+
+  this->declare_parameter("manual_timeout", 1.0);
+  double manualTimeout;
+  this->get_parameter("manual_timeout", manualTimeout);
+  this->_status.SetTimeout(rclcpp::Duration::from_seconds(manualTimeout));
+
+  this->declare_parameter("autonomous_timeout", 1.0);
+  double autonomousTimeout;
+  this->get_parameter("autonomous_timeout", autonomousTimeout);
+  this->_status.SetTimeout(rclcpp::Duration::from_seconds(autonomousTimeout));
+
+  this->declare_parameter("control_period", 0.01);
+  double controlPeriod;
+  this->get_parameter("control_period", controlPeriod);
+  // this->_timer = 
+  const auto temp = this->create_wall_timer(std::chrono::duration<double>(controlPeriod), std::bind(&SystemController::controlCallback, this));
 }
 
 void SystemController::bumperCallback(const create_msgs::msg::Bumper::ConstSharedPtr msg)
@@ -57,7 +77,17 @@ bool isStopCondition(const RobotStatus& status)
     return bumper.is_left_pressed || bumper.is_right_pressed || cliff.is_cliff_left || cliff.is_cliff_right || cliff.is_cliff_front_left || cliff.is_cliff_front_right;
 }
 
+void SystemController::manualCmdVelCallback(const geometry_msgs::msg::Twist::ConstSharedPtr msg)
+{
+  this->_manualCmd.SetData(*msg);
+}
+
 void SystemController::cmdVelCallback(const geometry_msgs::msg::Twist::ConstSharedPtr msg)
+{
+  this->_autonomousCmd.SetData(*msg);
+}
+
+void SystemController::controlCallback()
 {
   auto toSend = std::make_unique<geometry_msgs::msg::Twist>();
 
@@ -67,12 +97,26 @@ void SystemController::cmdVelCallback(const geometry_msgs::msg::Twist::ConstShar
     return;
   }
 
-  toSend->angular.z = msg->angular.z;
+  if (!this->_manualCmd.IsDataTimeout())
+  {
+    this->_cmdVelPub->publish(this->_manualCmd.GetData());
+    return;
+  }
+
+  if (this->_autonomousCmd.IsDataTimeout())
+  {
+    this->_cmdVelPub->publish(std::move(toSend));
+    return;
+  }
+
+  const auto msg = this->_autonomousCmd.GetData();
+
+  toSend->angular.z = msg.angular.z;
   const auto status = this->_status.GetData();
 
   if (isStopCondition(status))
   {
-    toSend->linear.x = std::min(0.0, msg->linear.x);
+    toSend->linear.x = std::min(0.0, msg.linear.x);
     this->_cmdVelPub->publish(std::move(toSend));
     return;
   }
@@ -80,11 +124,12 @@ void SystemController::cmdVelCallback(const geometry_msgs::msg::Twist::ConstShar
   if (isWarningZone(status))
   {
     RCLCPP_INFO(this->get_logger(), "WARNING SPEED");
-    toSend->linear.x = std::min(WarningSpeed, msg->linear.x);
+    toSend->linear.x = std::min(WarningSpeed, msg.linear.x);
     this->_cmdVelPub->publish(std::move(toSend));
     return;
   }
 
-  toSend->linear.x = msg->linear.x;
+  toSend->linear.x = msg.linear.x;
   this->_cmdVelPub->publish(std::move(toSend));
 }
+
