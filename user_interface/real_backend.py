16591733788683a5188071affa891a2361898cae
Owen Hooper <17ofh@queensu.ca>
2022-11-30 10:41:41 -0500

    Fixed real backend

--- user_interface/real_backend.py
+++ user_interface/real_backend.py
@@ -1,18 +1,41 @@
 import rclpy
+import time
+from rclpy import node
 from roomba_msgs.srv import GetAvailableDestinations
 from std_msgs.msg import Empty
 
 # Fake backend
 
-node = rclpy.Node('user_interface')
+rclpy.init()
+node = rclpy.create_node('user_interface')
 roomByFloor = {}
 obstacleList = ["Ramp", "Elevator"]
+rclpy.spin_once(node, timeout_sec=0.5)
 
-destinationClient = node.create_client(GetAvailableDestinations, 'available_destinations')
+destinationClient = node.create_client(GetAvailableDestinations, '/available_destinations')
 
-availDest = destinationClient.call(Empty())
-for dest in availDest.destinations:
-    roomByFloor[dest.floor_id.data].append(dest.label.data)
+print(node.get_service_names_and_types())
+
+destinationClient.wait_for_service()
+
+req = destinationClient.srv_type.Request()
+availDest = destinationClient.call_async(req)
+
+def callback(aDest):
+    res = aDest.result()
+    for dest in res.destinations:
+        if not str(dest.floor_id.data) in roomByFloor:
+            roomByFloor[str(dest.floor_id.data)] = []
+        roomByFloor[dest.floor_id.data].append(dest.label.data)
+    print(roomByFloor)
+
+availDest.add_done_callback(callback)
+
+i = 0
+
+while not availDest.done() or i < 10:
+    rclpy.spin_once(node, timeout_sec=0.1)
+    i = i + 1
 
 def getRoomsList():
     roomList = []
@@ -21,9 +44,8 @@ def getRoomsList():
     return roomList
 
 def listByFloor(floor):
-    return roomByFloor[floor]
+    return roomByFloor[str(floor)]
 
 def getObstackeList():
     return obstacleList
 
-rclpy.spin(node)
