6aa98a91b3aaccf7a3f3553918427f9d9478cc3f
Owen Hooper <17ofh@queensu.ca>
2023-04-09 16:45:27 -0400

    Removed things specific to my capstone project

--- dummy_elevator_traverser/src/dummy_elevator_traverser_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "dummy_elevator_traverser/DummyElevatorTraverser.hpp"
-int main(int argc, char** argv)
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DummyElevatorTraverser>("elevator_traverser"));
-  rclcpp::shutdown();
-  return 0;
-}
--- elevator_traverser/src/elevator_traverser_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "elevator_traverser/ElevatorTraverser.hpp"
-int main(int argc, char** argv)
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ElevatorTraverser>("elevator_traverser"));
-  rclcpp::shutdown();
-  return 0;
-}
